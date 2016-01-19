c80e312f62e2287c98f32e9234114efc8146a5ce
Jay Mixa <jmixa@gmail.com>
2016-01-18 22:16:00 -0600

    added features

--- app/controllers/fishes_controller.rb
+++ app/controllers/fishes_controller.rb
@@ -1,14 +1,60 @@
 class FishesController < ApplicationController
 
-  def fish
-    @fish = Fish.fish
-  end
 
-  def all_fish
+  def index
     @fishes = Fish.all
   end
 
+  def show
+    @fish = Fish.find_by(id: params[:id])
+  end
+#def new stays empty
+  def new
+  end
 
+  def create
+    @fish = Fish.create({
+      name: params[:name], 
+      price: params[:price], 
+      image: params[:image], 
+      description: params[:description]
+      })
+
+    flash[:success] = "New Item Added"
+
+    redirect_to "/"
+  end
+
+  def edit
+    @fish = Fish.find(params[:id])
+  end
+
+  def update
+    @fish = Fish.find(params[:id])
+    
+
+    @fish.update({
+      name: params[:name], 
+      price: params[:price], 
+      image: params[:image], 
+      description: params[:description]
+       })
+
+      
+
+      flash[:success] = "Item Updated"
+      redirect_to "/fishes/#{@fish.id}"
+  end
+
+  def destroy
+    @fish = Fish.find(params[:id])
+    @fish.destroy
+
+    flash[:success] = "You successfully removed an item."
+    flash[:warning] = "You will remove this item. "
+
+    redirect_to "/"
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
- get '/available' => 'fishes#fish'
+  get '/' => 'fishes#index' 
+  get '/fishes' => 'fishes#index'
 
- get '/all_available' => 'fishes#all_fish' 
+  get '/fishes/new' => "fishes#new"
+  post '/fishes' => 'fishes#create'
+
+
+  get '/fishes/:id' => 'fishes#show'
+
+  get '/fishes/:id/edit' => 'fishes#edit'
+  patch '/fishes/:id' => 'fishes#update'
+
+  delete '/fishes/:id' => 'fishes#destroy'
 
 end
